e4a04b42d36ccb89ed1feee0f68d163daf752953
tejji <tejji@tejji.com>
2013-12-25 22:23:59 -0800

    zpanelx64

---
+++ zpanelx64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+cd
+mkdir setup
+cd setup
+
+wget http://www.zvps.co.uk/downloads/centos/10-1-0/installer-10-1-0-centos-64.sh.x.tar.gz
+tar -xf installer-10-1-0-centos-64.sh.x.tar.gz
+chmod +x installer-10-1-0-centos-64.sh.x
+yum install ld-linux.so.2 curl
+./installer-10-1-0-centos-64.sh.x
+
+setso --set server_ip 000.000.000.000
