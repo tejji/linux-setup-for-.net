4331a807cab86983fe3461792a084b34ca0597e7
tejji <tejji@tejji.com>
2013-12-26 11:33:31 -0800

    using xsp 3.0.11 from github

--- mono-3.2.3-manual.sh
+++ mono-3.2.3-manual.sh
@@ -17,16 +17,22 @@ echo export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH>>~/.bash_profile
 echo export PATH=/usr/bin:$PATH>>~/.bash_profile
 source ~/.bash_profile
 
+
+# using latest verion of xsp from github https://github.com/mono/xsp/releases. This works for targetFramework=4.5
 cd /root/setup
-wget http://origin-download.mono-project.com/sources/xsp/xsp-2.10.2.tar.bz2
-tar xjf xsp-2.10.2.tar.bz2
-chown root:root xsp-2.10.2
-cd xsp-2.10.2
+wget https://github.com/mono/xsp/archive/3.0.11.tar.gz
+cp 3.0.11 3.0.11.tar.gz
+rm -f -r 3.0.11
+tar xvzf 3.0.11.tar.gz
+chown root:root xsp-3.0.11
+cd xsp-3.0.11
 chown -R root:root *
+./autogen.sh
+export PKG_CONFIG_PATH=/usr/lib/pkgconfig
 ./configure --prefix=/usr
-export PKG_CONFIG_PATH=`whereis pkgconfig | awk '{print $2}'`
 make
 make install
+
 
 cd /root/setup
 wget http://origin-download.mono-project.com/sources/libgdiplus/libgdiplus-2.10.9.tar.bz2
@@ -48,5 +54,17 @@ chown -R root:root *
 make
 make install
 ldconfig
+
+# vi /etc/httpd/conf/mod_mono.conf 
+## go to end of file and add following line 
+# MonoServerPath "/usr/bin/mod-mono-server4"
+
+## may have to edit "/usr/bin/xsp4" as exec /usr/bin/mono $MONO_OPTIONS "/usr/lib/mono/4.5/xsp4.exe" "$@"
+# cp /usr/lib/mono/4.0/xsp4.exe /usr/lib/mono/4.5/
+## may have to edit /usr/bin/mod-mono-server4  as exec /usr/bin/mono $MONO_OPTIONS "/usr/lib/mono/4.5/mod-mono-server4.exe" "$@"
+# cp /usr/lib/mono/4.0/mod-mono-server4.exe /usr/lib/mono/4.5/
+
+
+
 
 #service httpd restart
