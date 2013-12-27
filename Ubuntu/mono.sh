ed8a18be1c1c011f0e27d04163413dbd6f6ef7c4
tejji <tejji@tejji.com>
2013-12-26 17:39:22 -0800

    Update mono.sh

--- Ubuntu/mono.sh
+++ Ubuntu/mono.sh
@@ -1,35 +1,49 @@
+apt-get update
+apt-get upgrade -y
 apt-get install autoconf automake libtool g++ gettext
 apt-get install mono-gmcs
 apt-get install git
 apt-get install apache2-threaded-dev
+
+# sudo apt-get install -y build-essential libc6-dev g++ gcc libglib2.0-dev pkg-config git-core apache2 apache2-threaded-dev bison gettext autoconf automake libtool libpango1.0-dev libatk1.0-dev libgtk2.0-dev libtiff4-dev libgif-dev libglade2-dev curl
+
+PREFIX=/usr
+export PATH=$PREFIX/bin:$PATH
+export PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
+
+#echo export PKG_CONFIG_PATH=/usr/lib/pkgconfig:$PKG_CONFIG_PATH>>~/.bash_profile
+#echo export PATH=/usr/bin:$PATH>>~/.bash_profile
+#source ~/.bash_profile
 
 cd /opt
 mkdir mono-3.2.5
 cd /opt/mono-3.2.5
 
 git clone git://github.com/mono/mono.git
-git clone git://github.com/mono/xps.git
+git clone git://github.com/mono/xsp.git
 git clone git://github.com/mono/libgdiplus.git
 git clone git://github.com/mono/mod_mono.git
 
 cd /opt/mono-3.2.5/libgdiplus
-./autogen.sh --prefix=/usr
+./autogen.sh --prefix=$PREFIX
 make
 make install
 
 cd /opt/mono-3.2.5/mono/
 make clean
-./autogen.sh --prefix=/usr 
+./autogen.sh --prefix=$PREFIX
 make
 make install
 
 cd /opt/mono-3.2.5/xsp
-./autogen.sh --prefix=/usr
+./autogen.sh --prefix=$PREFIX
 make
 make install
 
 cd /opt/mono-3.2.5/mod_mono
-./autogen.sh --prefix=/usr
+./autogen.sh --prefix=$PREFIX
 make
 make install
 
