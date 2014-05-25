8785a0604aeca5426b601cee450a18178f6a0377
tejji <tejji@tejji.com>
2014-05-25 13:55:34 -0700

    updated piwik

--- Ubuntu/piwik/setup-piwik.sh
+++ Ubuntu/piwik/setup-piwik.sh
@@ -9,6 +9,11 @@
 # MANUAL - setup the piwik domain manually by logging into zpanel
 # MANUAL - change the password of mysql to desired password using zpanel
 # MANUAL - change the password of mysql at /etc/zpanel/panel/cnf/db.php
+# MANUAL - change the password of mysql at /etc/zpanel/panel/etc/apps/webmail/config/db.inc.php
+# MANUAL - change the password of mysql at /etc/zpanel/configs/proftpd/proftpd-mysql.conf
+
+# MANUAL - wget https://raw.githubusercontent.com/tejji/linux-setup-for-.net/master/Ubuntu/piwik/setup-piwik.sh
+# MANUAL - sudo bash setup-piwik.sh
 
 apt-get update
 apt-get upgrade
@@ -17,7 +22,16 @@ apt-get install unzip php5-gd
 cd /var/zpanel/hostdata/zadmin/public_html/piwik_tejji_net
 wget http://builds.piwik.org/latest.zip
 unzip latest.zip
-chown -R www-data:www-data /var/zpanel/hostdata/zadmin/public_html/piwik_tejji_net/piwik
+
+rm *html *zip
+#cd piwik
+#rm README.md LEAGALNOTICE
+#cd misc
+#rm How\ to\ install\ Piwik.html gpl-3.0.txt
+#cd ..
+#cd ..
+
+chown -R www-data:www-data /var/zpanel/hostdata/zadmin/public_html/piwik_tejji_net/
 service apache2 restart
 
 # http://<IP-address-OR-domain>/piwik/ - MANUAL - complete rest of the installation here
