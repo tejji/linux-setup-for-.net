4c4079c91e9164543694bf6392147fcd8d55c76f
tejji <tejji@users.noreply.github.com>
2014-07-02 10:53:22 -0700

    Update mono-with-panel.sh

--- Ubuntu/mono-with-panel.sh
+++ Ubuntu/mono-with-panel.sh
@@ -18,3 +18,27 @@ apt-get install mono-complete -y
 apt-get install libapache2-mod-mono mono-apache-server2 -y
 # apt-get install mono-fastcgi-server4 -y
 
+# Instructions
+# vim /etc/apache2/apache2.conf
+# Add Include /etc/apache2/mods-enabled/mod_mono.conf at the end of the file (without quotes!)
+
+# http://go-mono.com/config-mod-mono/Default.aspx >> VirtualHost >> fill form >> Preview >> copy from MonoServerPath and paste it into 
+# should include following
+# Alias /site.name "/var/zpanel/hostdata/zadmin/public_html/site_name"
+# MonoServerPath site.name "/usr/bin/mod-mono-server4"
+# MonoDebug site.name true
+# MonoSetEnv site.name MONO_IOMAP=all
+# MonoApplications site.name "/:/var/zpanel/hostdata/zadmin/public_html/site_name" # <-- ENSURE THAT LOCATION IS  /site_name
+#  <Location "/"> # <-- ENSURE THAT LOCATION IS / INSTEAD OF /site_name
+#    Allow from all
+#    Order allow,deny
+#    MonoSetServerAlias site.name
+#    SetHandler mono
+#    SetOutputFilter DEFLATE
+#    SetEnvIfNoCase Request_URI "\.(?:gif|jpe?g|png)$" no-gzip dont-vary
+#  </Location>
+#  <IfModule mod_deflate.c>
+#    AddOutputFilterByType DEFLATE text/html text/plain text/xml text/javascript
+#  </IfModule>
+
+# sudo service apache2 restart
