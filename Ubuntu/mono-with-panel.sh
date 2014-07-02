e8a7c29ba488ddb11b12450b4ace4e91d8b9ecd4
tejji <tejji@users.noreply.github.com>
2014-07-02 08:59:40 -0700

    Update and rename mono-ajenti.sh to mono-with-panel.sh

---
+++ Ubuntu/mono-with-panel.sh
@@ -0,0 +1,16 @@
+# install Ajenti - http://support.ajenti.org/topic/349868-installing-on-ubuntu/
+# wget -O- https://raw.github.com/Eugeny/ajenti/master/scripts/install-ubuntu.sh | sudo sh
+
+# install Ajenti V - http://support.ajenti.org/topic/353398-installing-ajenti-v-on-debian/
+# apt-get remove apache2
+# apt-get install ajenti-v ajenti-v-nginx ajenti-v-mysql ajenti-v-php-fpm php5-mysql -y
+# service ajenti restart
+
+# install zpanel - http://docs.zpanelcp.com/?node=22
+bash <(curl -LSs https://raw.github.com/zpanel/installers/master/install/Ubuntu-12_04/10_1_1.sh)
+
+
+# install mono
+apt-get install mono-complete -y
+# apt-get install mono-fastcgi-server4 -y
+
