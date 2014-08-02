e29bca016011a8a7cfd8159931e9c6b58d357afa
tejji <tejji@users.noreply.github.com>
2014-08-02 12:08:54 -0700

    Update ajenti-setup.sh

--- Ubuntu/ajenti-setup.sh
+++ Ubuntu/ajenti-setup.sh
@@ -3,3 +3,6 @@ wget -O- https://raw.github.com/Eugeny/ajenti/master/scripts/install-ubuntu.sh |
 
 # Add Web Server
 # https://www.digitalocean.com/community/tutorials/how-to-install-ajenti-control-panel-on-ubuntu-13-04
+
+# Add Mail Server
+# apt-get install ajenti-v-mail
