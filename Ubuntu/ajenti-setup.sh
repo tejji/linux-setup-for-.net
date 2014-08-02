a4cd9abe2badac25edeaf9f9e560af9a6177629e
tejji <tejji@users.noreply.github.com>
2014-08-02 11:42:21 -0700

    Create ajenti-setup.sh

---
+++ Ubuntu/ajenti-setup.sh
@@ -0,0 +1,5 @@
+# install Ajenti
+wget -O- https://raw.github.com/Eugeny/ajenti/master/scripts/install-ubuntu.sh | sudo sh
+
+# Add Web Server
+# https://www.digitalocean.com/community/tutorials/how-to-install-ajenti-control-panel-on-ubuntu-13-04
