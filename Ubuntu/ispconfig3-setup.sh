06756bd418a319dd3403efcfc602a7421ad8bf07
tejji <tejji@users.noreply.github.com>
2014-08-02 17:53:43 -0700

    Update ispconfig3-setup.sh

--- Ubuntu/ispconfig3-setup.sh
+++ Ubuntu/ispconfig3-setup.sh
@@ -1,1 +1,8 @@
 #https://github.com/dclardy64/ISPConfig-3-Debian-Installer
+
+# setup squirrelmail
+#http://www.howtoforge.com/perfect-server-ubuntu-14.04-apache2-php-mysql-pureftpd-bind-dovecot-ispconfig-3-p6
+
+# changes in /etc/postfix/main.cf
+#local_recipient_maps =
+#mydestination = $myhostname, localhost, localhost.$mydomain, localhost.localdomain
